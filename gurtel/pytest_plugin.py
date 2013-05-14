8e635ec12f6f5023cfb357a2a76812b1ee402cb9
Carl Meyer <carl@oddbird.net>
2013-05-14 18:28:57 -0400

    Make configfile fixture available as part of a py.test plugin.

--- gurtel/pytest_plugin.py
+++ gurtel/pytest_plugin.py
@@ -6,6 +6,13 @@ import pytest
 
 @pytest.fixture
 def configfile(request, tmpdir):
+    """
+    Write a temporary config file.
+
+    Config file takes contents from ``configfile_contents`` mark, otherwise is
+    empty.
+
+    """
     if 'configfile_contents' in request.keywords:
         contents = request.keywords['configfile_contents'].args[0]
     else:
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_version():
     try:
         for line in fh.readlines():
             if line.startswith("__version__ ="):
-                return line.split("=")[1].strip().strip('"')
+                return line.split("=")[1].strip().strip('"\'')
     finally:
         fh.close()
 
@@ -47,4 +47,9 @@ setup(
         "pytz",
         "requests",
         ],
-)
+    entry_points = {
+        'pytest11': [
+            'gurtel = gurtel.pytest_plugin',
+            ]
+        },
+    )
